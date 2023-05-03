8a24f0f26397aaa0269e428dc2d779656db9f07e
Shadow1900 <118683823+Shadow1900@users.noreply.github.com>
2023-05-02 18:28:30 -0600

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
- 
+export class AppComponent {
+  tamano = 30;
+  presentacion = {
+    'background-color': 'black',
+    color: 'white',
+    'width.px': '1000',
+    'height.px': '200',
+    'font-size.px': this.tamano,
+    display: 'flex',
+    'justify-content': 'center',
+    'align-items': 'center',
+  };
+
+  agrandar() {
+    this.tamano++;
+    this.presentacion['font-size.px'] = this.tamano;
+  }
+
+  reducir() {
+    this.tamano--;
+    this.presentacion['font-size.px'] = this.tamano;
+  }
 }
